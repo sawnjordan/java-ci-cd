b1eca68704f4b04b1a43a52d63aefde2c2490309
sawnjordan <sawnjordan@gmail.com>
2024-11-28 20:03:12 +0545

    just checking if the workflow triggers or not

--- terraform-config/eks.tf
+++ terraform-config/eks.tf
@@ -20,7 +20,6 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = var.instance_types
