7cf7230e50ff2639093b134087ca3ec92f4c7aca
sawnjordan <sawnjordan@gmail.com>
2024-11-28 11:16:31 +0545

    simple java spring boot app

---
+++ src/main/java/com/example/task_demo/WelCome.java
@@ -0,0 +1,11 @@
+package com.example.task_demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class WelCome {
+    @RequestMapping("/welcome")
+    public String hello() {
+        return "WelCome to Java CI/CD.";
+    }
+}
